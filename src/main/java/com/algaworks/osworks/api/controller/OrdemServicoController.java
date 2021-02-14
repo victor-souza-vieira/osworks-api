a96939f4ba3e246f234d9d208661b19c156f8a3e
Victor Souza <victor.ti.souza@gmail.com>
2021-02-13 21:47:19 -0300

    DTO para ordem de servico com ModelMapper + listar e buscar ordens de servico

--- src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -1,14 +1,25 @@
 package com.algaworks.osworks.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.osworks.api.model.OrdemServicoModel;
 import com.algaworks.osworks.domain.model.OrdemServico;
+import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
 
 @RestController
@@ -18,10 +29,34 @@ public class OrdemServicoController {
 	@Autowired
 	private GestaoOrdemServicoService gestaoOrdemServico;
 	
+	@Autowired
+	private OrdemServicoRepository ordemServicoRespository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public OrdemServico criar(@RequestBody OrdemServico ordemServico) {
+	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
 		return gestaoOrdemServico.criar(ordemServico);
+	}
+	
+	@GetMapping
+	public List<OrdemServico> listar(){
+		return ordemServicoRespository.findAll();
+	}
+	
+	@GetMapping("/{ordemServicoId}")
+	public ResponseEntity<OrdemServicoModel> buscar(@PathVariable Long ordemServicoId) {
+		Optional<OrdemServico> ordemServico = ordemServicoRespository.findById(ordemServicoId);
+		
+		if(ordemServico.isPresent()) {
+			OrdemServicoModel ordemServicoModel = modelMapper.map(ordemServico.get(), OrdemServicoModel.class);
+			return ResponseEntity.ok(ordemServicoModel);
+		}
+		
+		return ResponseEntity.notFound().build();
+		
 	}
 	
 }
