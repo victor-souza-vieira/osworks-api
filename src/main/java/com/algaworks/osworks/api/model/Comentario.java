feb3c3448f0e429594df0f71f2162b68cf1032a1
Victor Souza <victor.ti.souza@gmail.com>
2021-02-15 21:49:49 -0300

    add class comentario

---
+++ src/main/java/com/algaworks/osworks/api/model/Comentario.java
@@ -0,0 +1,38 @@
+package com.algaworks.osworks.api.model;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.algaworks.osworks.domain.model.OrdemServico;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@EqualsAndHashCode(of = {"id"})
+public class Comentario {
+
+	@Getter
+	@Setter
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Getter
+	@Setter
+	@ManyToOne
+	private OrdemServico comentario;
+	@Getter
+	@Setter
+	private String descricao;
+	@Getter
+	@Setter
+	private OffsetDateTime dataEnvio;
+
+}
--- src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
+++ src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
@@ -2,6 +2,8 @@ package com.algaworks.osworks.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,13 +12,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.algaworks.osworks.api.model.Comentario;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@EqualsAndHashCode(of = {"id"})
+@EqualsAndHashCode(of = { "id" })
 public class OrdemServico {
 
 	@Id
@@ -24,30 +29,35 @@ public class OrdemServico {
 	@Getter
 	@Setter
 	private Long id;
-	
+
 	@ManyToOne
 	@Getter
 	@Setter
 	private Cliente cliente;
-	
+
 	@Getter
 	@Setter
 	private String descricao;
-	
+
 	@Getter
 	@Setter
 	private BigDecimal preco;
-	
+
 	@Enumerated(EnumType.STRING)
 	@Getter
 	@Setter
 	private StatusOrdemServico status;
-	
+
 	@Getter
 	@Setter
 	private OffsetDateTime dataAbertura;
-	
+
 	@Getter
 	@Setter
 	private OffsetDateTime dataFinalizacao;
+
+	@OneToMany(mappedBy = "ordemServico")
+	@Getter
+	@Setter
+	private List<Comentario> comentarios = new ArrayList<>();
 }
